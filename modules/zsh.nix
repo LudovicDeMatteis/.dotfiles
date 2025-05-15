72167373635519ecdd85966a75e9aed22d6c9d74
Ludovic De Matteis <ludovic.dematteis2@gmail.com>
2025-05-15 18:31:13 +0200

    Add better terminal management

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,6 +1,39 @@
 {
-  programs.zsh = {
+  programs.starship = {
     enable = true;
+    # custom settings
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+
+  programs.alacritty = {
+    enable = true;
+    # custom settings
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        size = 12;
+        draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    bashrcExtra = ''
+      export PATH="$PATH:$HOME/.nix-profile/bin"
+    '';
+  };
+
+  programs.zsh = {
+    enable = false;
     autocd = true;
     autosuggestion.enable = true;
     enableCompletion = true;
