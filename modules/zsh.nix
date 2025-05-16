ce4a2a1a1cc466e80bbd124a434a77585b4a2615
Ludovic De Matteis <ldematteis@laas.fr>
2025-05-16 13:34:45 +0200

    Update zsh config

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,42 +1,10 @@
 {
-  programs.starship = {
-    enable = true;
-    # custom settings
-    settings = {
-      add_newline = false;
-      aws.disabled = true;
-      gcloud.disabled = true;
-      line_break.disabled = true;
-    };
-  };
-
-  programs.alacritty = {
-    enable = true;
-    # custom settings
-    settings = {
-      env.TERM = "xterm-256color";
-      font = {
-        size = 12;
-        draw_bold_text_with_bright_colors = true;
-      };
-      scrolling.multiplier = 5;
-      selection.save_to_clipboard = true;
-    };
-  };
-
-  programs.bash = {
-    enable = true;
-    enableCompletion = true;
-    bashrcExtra = ''
-      export PATH="$PATH:$HOME/.nix-profile/bin"
-    '';
-  };
-
   programs.zsh = {
-    enable = false;
+    enable = true;
     autocd = true;
-    autosuggestion.enable = true;
     enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
     shellAliases = {
       # Aliases for common commands
       ll = "ls -l";
@@ -45,9 +13,15 @@
     # Enable Oh My Zsh
     oh-my-zsh = {
       enable = true;
-      theme = "agnoster";
+      theme = "robbyrussell";
       plugins = [
+        "git" "thefuck"
       ];
     };
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
   };
 }
